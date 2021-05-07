86b019f8b7e245325cf876426b8edb053cfe0ffe
Enias Oliveira <eniasoliveira27@gmail.com>
2021-05-07 16:36:06 -0300

    feat: adds authentication configurations

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -7,10 +7,20 @@ class UserSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     username = serializers.CharField()
     password = serializers.CharField(write_only=True)
-    is_staff = serializers.BooleanField()
     is_superuser = serializers.BooleanField()
+    is_staff = serializers.BooleanField()
 
     def validate_username(self, value):
         if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError()
+        return value
+
+
+class CredentialSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate_username(self, value):
+        if not User.objects.filter(username=value).exists():
             raise serializers.ValidationError()
         return value
--- authentication/views.py
+++ authentication/views.py
@@ -1,11 +1,13 @@
 from rest_framework import status
 
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
+from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .serializers import UserSerializer
+from .serializers import UserSerializer, CredentialSerializer
 
 
 class AccountView(APIView):
@@ -25,3 +27,20 @@ class AccountView(APIView):
         serialized_user = UserSerializer(user)
 
         return Response(serialized_user.data, status=status.HTTP_201_CREATED)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        serialized_request = CredentialSerializer(data=request.data)
+
+        if not serialized_request.is_valid():
+            return Response(
+                serialized_request.errors, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        user = authenticate(
+            username=request.data["username"],
+            password=request.data["password"],
+        )
+        token = Token.objects.get_or_create(user=user)[0]
+        return Response({"token": token.key})
